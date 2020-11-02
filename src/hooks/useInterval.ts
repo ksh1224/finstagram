4482d18ac7cf26c43022a04d93071586a3ac760c
권수현 <3a9lyd@naver.com>
2020-11-02 10:08:13 +0900

    훅 추가(인터벌, 이전 값 사용) 추가

---
+++ src/hooks/useInterval.ts
@@ -0,0 +1,19 @@
+/* eslint-disable consistent-return */
+import { useEffect, useRef } from "react";
+
+export default function useInterval(callback: () => void, delay: number) {
+  const savedCallback = useRef(callback);
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
