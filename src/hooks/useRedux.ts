e0c57157118e2af6329b959d8af9f67a615d4b61
권수현 <3a9lyd@naver.com>
2020-11-02 10:11:42 +0900

    리덕스 훅 추가

---
+++ src/hooks/useRedux.ts
@@ -0,0 +1,70 @@
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  feedbackMainActionAsync,
+  feedRecentActionAsync,
+  searchUserActionAsync,
+} from "../store/actions";
+import { RootState } from "../store";
+
+export function useAuth() {
+  const Auth = useSelector((state: RootState) => state.Auth);
+
+  const APIAuth = useSelector((state: RootState) => state.APIAuth);
+
+  return {
+    Auth,
+    APIAuth,
+  };
+}
+
+export function useSearchUser() {
+  const { isFetching, data } = useSelector(
+    (state: RootState) => state.searchUser
+  );
+  const dispatch = useDispatch();
+
+  const request = useCallback(() => dispatch(searchUserActionAsync.request()), [
+    dispatch,
+  ]);
+
+  return {
+    request,
+    data,
+    isFetching,
+  };
+}
+
+export function useFeedbackMain() {
+  const { isFetching, data } = useSelector(
+    (state: RootState) => state.feedbackMain
+  );
+  const dispatch = useDispatch();
+
+  const request = useCallback(
+    () => dispatch(feedbackMainActionAsync.request()),
+    [dispatch]
+  );
+  return {
+    request,
+    data,
+    isFetching,
+  };
+}
+
+export function useFeedRecent() {
+  const { isFetching, data } = useSelector(
+    (state: RootState) => state.feedRecent
+  );
+  const dispatch = useDispatch();
+
+  const request = useCallback(
+    (page) => dispatch(feedRecentActionAsync.request(page)),
+    [dispatch]
+  );
+  return {
+    request,
+    data,
+    isFetching,
+  };
+}
