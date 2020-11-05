789871a2207b00aa096596e5079d275c922daa58
권수현 <3a9lyd@naver.com>
2020-11-05 18:49:58 +0900

    유틸 추가 및 수정(검색 유틸 추가)

---
+++ src/utils/searchUtil.ts
@@ -0,0 +1,47 @@
+/* eslint-disable no-restricted-syntax */
+import hangul from "hangul-js";
+
+export function search(keyword: string, text: string) {
+  const disassembleGruop = hangul.d(keyword, true);
+  let keywordCho = "";
+  disassembleGruop.forEach((gruop) => {
+    keywordCho += gruop[0] || "";
+  });
+  if (
+    keyword.toUpperCase().includes(text.toUpperCase()) ||
+    keywordCho.toUpperCase().includes(text.toUpperCase())
+  ) {
+    return 1;
+  }
+  return 0;
+}
+
+export function searchList(
+  data: { id: number; name: string; organizationName: string }[],
+  searchText: string,
+  filterUser?: { id: number }[]
+) {
+  if (!searchText || searchText.trim() === "" || searchText.trim().length < 2)
+    return [];
+  const nameList: { name: string; organizationName: string }[] = [];
+  const organizationNameList: { name: string; organizationName: string }[] = [];
+  for (const obj of data) {
+    let include = false;
+    if (filterUser && filterUser.length !== 0) {
+      include = !!filterUser.find(({ id }) => id === obj.id);
+    }
+    if (!include)
+      if (search(obj.name, searchText)) {
+        const findIndex = nameList.findIndex((item) => item.name > obj.name);
+        if (findIndex === -1) nameList.push(obj);
+        else nameList.splice(findIndex, 0, obj);
+      } else if (search(obj.organizationName, searchText)) {
+        const findIndex = organizationNameList.findIndex(
+          (item) => item.name > obj.name
+        );
+        if (findIndex === -1) organizationNameList.push(obj);
+        else organizationNameList.splice(findIndex, 0, obj);
+      }
+  }
+  return [...nameList, ...organizationNameList];
+}
