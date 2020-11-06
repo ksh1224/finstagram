1511e078ffe3638e2d85db02a45920e69bec77d6
권수현 <3a9lyd@naver.com>
2020-11-06 15:57:02 +0900

    코멘트 사가 통합

--- src/store/saga/commentSaga.ts
+++ src/store/saga/commentSaga.ts
@@ -1,7 +1,13 @@
 import { call, put, select, take } from "redux-saga/effects";
 
 import axios from "utils/axiosUtil";
-import { commentActionAsync, feedRecentActionAsync } from "store/actions";
+import {
+  commentActionAsync,
+  commentDeleteActionAsync,
+  commentLikeActionAsync,
+  commentNewActionAsync,
+  commentUpdateActionAsync,
+} from "store/actions";
 
 export default function* commentSaga(
   feedId: number
@@ -32,5 +38,77 @@ export default function* commentSaga(
     }
   } catch (error) {
     yield put(commentActionAsync.failure(error));
+  }
+}
+
+export function* commentNewSaga(
+  year?: number,
+  querter?: number
+): Generator<any, void, ObjectType> {
+  try {
+    const data = yield call(
+      axios,
+      `/feedbacks/sent${year ? `?year=${year}` : ""}${
+        querter ? `${year ? "&" : "?"}querter=${querter}` : ""
+      }`,
+      "GET"
+    );
+    yield put(commentNewActionAsync.success(data));
+  } catch (error) {
+    yield put(commentNewActionAsync.failure(error));
+  }
+}
+
+export function* commentLikeSaga(
+  year?: number,
+  querter?: number
+): Generator<any, void, ObjectType> {
+  try {
+    const data = yield call(
+      axios,
+      `/feedbacks/sent${year ? `?year=${year}` : ""}${
+        querter ? `${year ? "&" : "?"}querter=${querter}` : ""
+      }`,
+      "GET"
+    );
+    yield put(commentLikeActionAsync.success(data));
+  } catch (error) {
+    yield put(commentLikeActionAsync.failure(error));
+  }
+}
+
+export function* commentUpdateSaga(
+  year?: number,
+  querter?: number
+): Generator<any, void, ObjectType> {
+  try {
+    const data = yield call(
+      axios,
+      `/feedbacks/sent${year ? `?year=${year}` : ""}${
+        querter ? `${year ? "&" : "?"}querter=${querter}` : ""
+      }`,
+      "GET"
+    );
+    yield put(commentUpdateActionAsync.success(data));
+  } catch (error) {
+    yield put(commentUpdateActionAsync.failure(error));
+  }
+}
+
+export function* commentDeleteSaga(
+  year?: number,
+  querter?: number
+): Generator<any, void, ObjectType> {
+  try {
+    const data = yield call(
+      axios,
+      `/feedbacks/sent${year ? `?year=${year}` : ""}${
+        querter ? `${year ? "&" : "?"}querter=${querter}` : ""
+      }`,
+      "GET"
+    );
+    yield put(commentDeleteActionAsync.success(data));
+  } catch (error) {
+    yield put(commentDeleteActionAsync.failure(error));
   }
 }
