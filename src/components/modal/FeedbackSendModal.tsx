6e6be7e5c4bd04602d4bdd9008500cd9a87380e5
권수현 <3a9lyd@naver.com>
2020-11-06 15:58:34 +0900

    피드백 보내기 api 연동

--- src/components/modal/FeedbackSendModal.tsx
+++ src/components/modal/FeedbackSendModal.tsx
@@ -2,18 +2,37 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import Profile from "components/Profile";
-import { useModal, useBadgeList } from "hooks/useRedux";
-import React, { useEffect, useState } from "react";
+import { useModal, useBadgeList, useFeedback } from "hooks/useRedux";
+import React, { useEffect, useState, createRef } from "react";
 
 export default function FeedbackModal() {
+  const fileRef = createRef<HTMLInputElement>();
+  const textLimit = 100;
   const [type, setType] = useState<"PRAISE" | "ADVICE">("PRAISE");
-  const [select, setSelect] = useState<number>(0);
+  const [select, setSelect] = useState(0);
+  const [contents, setContents] = useState("");
+  const [file, setFile] = useState<any>(null);
+  const { feedbackSend } = useFeedback();
   const { feedbackUser, isShowFeedback } = useModal();
-  const { data } = useBadgeList();
+  const { data: dadgeList } = useBadgeList();
+
+  useEffect(() => {
+    console.log("file", file);
+  }, [file]);
+
+  function sendFeedback() {
+    feedbackSend(
+      type,
+      feedbackUser,
+      dadgeList?.CONTRIBUTION[select],
+      contents,
+      file && file[0]
+    );
+  }
 
   return (
     <div
-      className="modal fade"
+      className={`modal fade ${feedbackUser && "show"}`}
       id="modal_sendFeedback"
       tabIndex={-1}
       role="dialog"
@@ -85,7 +104,7 @@ export default function FeedbackModal() {
                 <div>
                   <div className="feedback-icon-group w-100">
                     <div className="text-nowrap d-flex justify-content-between">
-                      {data?.CONTRIBUTION?.map((badge: any, i: number) => (
+                      {dadgeList?.CONTRIBUTION?.map((badge: any, i: number) => (
                         <div
                           key={badge?.id}
                           className="d-inline-block text-center"
@@ -118,13 +137,21 @@ export default function FeedbackModal() {
 
                   <div className="d-flex justify-content-between">
                     <p>동료에 대해 칭찬하고 싶은 내용을 작성해주세요.</p>
-                    <span className="text-muted font-size-sm">0/100</span>
+                    <span className="text-muted font-size-sm">{`${contents.length}/${textLimit}`}</span>
                   </div>
 
                   <textarea
                     className="form-control resize-none"
                     id="exampleTextarea"
                     rows={5}
+                    value={contents}
+                    onChange={({ target }) =>
+                      setContents(
+                        target.value.length <= textLimit
+                          ? target.value
+                          : target.value.substr(0, textLimit)
+                      )
+                    }
                   />
 
                   <ol
@@ -149,13 +176,21 @@ export default function FeedbackModal() {
                       &apos;협업&apos;또는 &apos;성과추진활동&apos;과 관련해
                       조언해주고 싶은 내용을 작성해주세요.
                     </p>
-                    <span className="text-muted font-size-sm">0/100</span>
+                    <span className="text-muted font-size-sm">{`${contents.length}/${textLimit}`}</span>
                   </div>
 
                   <textarea
                     className="form-control resize-none"
                     id="exampleTextarea"
                     rows={5}
+                    value={contents}
+                    onChange={({ target }) =>
+                      setContents(
+                        target.value.length <= textLimit
+                          ? target.value
+                          : target.value.substr(0, textLimit)
+                      )
+                    }
                   />
 
                   <ol
@@ -163,7 +198,7 @@ export default function FeedbackModal() {
                     style={{ listStylePosition: "inside" }}
                   >
                     <li>
-                      구체적인 행동(관찰 가능한 행동)에 초점을 맞추어 조언해
+                      구체적인 행동(관찰 능한 행동)에 초점을 맞추어 조언해
                       주세요.
                     </li>
                     <li>
@@ -172,53 +207,65 @@ export default function FeedbackModal() {
                       *Feedback 내용은 선택하신 동료만 확인 가능합니다.
                     </li>
                   </ol>
-
+                  <input
+                    ref={fileRef}
+                    type="file"
+                    onChange={({ target }) => setFile(target.files)}
+                    style={{ display: "none" }}
+                  />
                   <div className="dropzone dropzone-multi mt-6 attch_file">
                     <div className="dropzone-panel mb-lg-0 mb-2">
-                      <a className="dropzone-select btn btn-light-primary font-weight-bold btn-sm">
+                      <a
+                        className="dropzone-select btn btn-light-primary font-weight-bold btn-sm"
+                        onClick={() => fileRef.current?.click()}
+                      >
                         파일 첨부
                       </a>
                     </div>
                     <div className="dropzone-items">
-                      <div
-                        className="dropzone-item"
-                        style={{ display: "none" }}
-                      >
-                        <div className="dropzone-file">
-                          <div
-                            className="dropzone-filename"
-                            title="some_image_file_name.jpg"
-                          >
-                            <span data-dz-name="">
-                              some_image_file_name.jpg
-                            </span>
-                            <strong>
-                              (<span data-dz-size="">340kb</span>)
-                            </strong>
-                          </div>
-                          <div
-                            className="dropzone-error"
-                            data-dz-errormessage=""
-                          />
-                        </div>
-                        <div className="dropzone-progress">
-                          <div className="progress">
+                      {!!file && !!file[0] && (
+                        <div className="dropzone-item">
+                          <div className="dropzone-file">
                             <div
-                              className="progress-bar bg-primary"
-                              role="progressbar"
-                              aria-valuemin={0}
-                              aria-valuemax={100}
-                              aria-valuenow={0}
-                              data-dz-uploadprogress=""
+                              className="dropzone-filename"
+                              title={file[0].name}
+                            >
+                              <span data-dz-name="">{file[0].name}</span>
+                              <strong>
+                                (
+                                <span data-dz-size={file[0].size}>
+                                  {`${file[0].size}KB`}
+                                </span>
+                                )
+                              </strong>
+                            </div>
+                            <div
+                              className="dropzone-error"
+                              data-dz-errormessage=""
                             />
                           </div>
+                          <div className="dropzone-progress">
+                            <div className="progress">
+                              <div
+                                className="progress-bar bg-primary"
+                                role="progressbar"
+                                aria-valuemin={0}
+                                aria-valuemax={100}
+                                aria-valuenow={0}
+                                data-dz-uploadprogress=""
+                              />
+                            </div>
+                          </div>
+                          <div className="dropzone-toolbar">
+                            <span className="dropzone-delete" data-dz-remove="">
+                              <i
+                                className="flaticon2-cross"
+                                onClick={() => setFile(null)}
+                              />
+                            </span>
+                          </div>
                         </div>
-                        <div className="dropzone-toolbar">
-                          <span className="dropzone-delete" data-dz-remove="">
-                            <i className="flaticon2-cross" />
-                          </span>
-                        </div>
-                      </div>
+                      )}
                     </div>
                   </div>
                 </div>
@@ -230,6 +277,7 @@ export default function FeedbackModal() {
             <button
               type="button"
               className="btn btn-lg btn-primary w-100 m-0 rounded-0"
+              onClick={() => sendFeedback()}
             >
               보내기
             </button>
