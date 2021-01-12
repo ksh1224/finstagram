eb588b7f47602f7e22bc6ffede8196ccdf42fc56
권수현 <3a9lyd@naver.com>
2021-01-13 01:30:17 +0900

    모달 액션 추가

--- src/store/actions/ModalAction.ts
+++ src/store/actions/ModalAction.ts
@@ -3,8 +3,25 @@ import { createAction } from "typesafe-actions";
 export type ModalNameType =
   | "requestFeedback"
   | "sendFeedback"
+  | "updateRequestFeedback"
+  | "updateSendFeedback"
   | "topRanker"
-  | "okrHistory";
+  | "okrHistory"
+  | "okrComment"
+  | "commentUpdate"
+  | "userProfile"
+  | "okrUpdate"
+  | "selfReview"
+  | "teamReview"
+  | "peerReview"
+  | "addReviewer"
+  | "addReviewerComment"
+  | "addTeamReviewer"
+  | "leaderReview"
+  | "okrSelfReview"
+  | "okrTeamReview"
+  | "feedback"
+  | "help";
 
 export const showModalAction = createAction(
   "SHOW_MODAL",
