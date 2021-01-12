ac906296aa76ba553b1d71e3deb94c12dccfaa2a
권수현 <3a9lyd@naver.com>
2021-01-13 01:44:45 +0900

    팀, 유저 okr 영역 컴포넌트 수정

--- src/components/okr/TeamOKR.tsx
+++ src/components/okr/TeamOKR.tsx
@@ -2,12 +2,20 @@ import Profile from "components/Profile";
 import { useAuth } from "hooks/useRedux";
 import { useTeamOKR } from "hooks/useOKRRedux";
 import React, { useEffect, useState } from "react";
+import DataValidationContainer from "layouts/DataValidationContainer";
 import OKRAccordion from "./OKRAccordion";
 import OKRGraph from "./OKRGraph";
 
 export default function TeamOKR() {
-  const { data: responseData = {}, isFetching, requset } = useTeamOKR();
-  const { data, availableDates, year, quarter } = responseData;
+  const { data: responseData = {}, isFetching, request } = useTeamOKR();
+  const {
+    data,
+    availableDates,
+    year,
+    quarter,
+    organizationId,
+    organizationName,
+  } = responseData;
   const memberData = data?.data;
   const { user: my } = useAuth();
   const [show, setShow] = useState(false);
@@ -18,7 +26,7 @@ export default function TeamOKR() {
 
   function changeDate(changeYear: number, changeQuarter: number) {
     setShow(false);
-    requset(changeYear, changeQuarter);
+    request(changeYear, changeQuarter, organizationId, organizationName);
     setTimeout(() => setShow(true), 200);
   }
 
@@ -26,7 +34,9 @@ export default function TeamOKR() {
     <div className="section-2 col-auto h-sm-100 flex-grow-1 w-100px d-flex flex-column px-0">
       <div className="card card-custom card-stretch rounded-bottom-0 shadow-none">
         <div className="card-header border-0">
-          <h3 className="card-title font-weight-bolder">Team OKR</h3>
+          <h3 className="card-title font-weight-bolder">
+            {organizationName || "Team OKR"}
+          </h3>
           <div className="card-toolbar">
             <select
               onChange={({ target }) => {
@@ -50,43 +60,50 @@ export default function TeamOKR() {
           </div>
         </div>
         <div className="card-body pt-2 overflow-y-auto">
-          <div className="gutter-t mb-10">
-            <div className="d-flex justify-content-between align-items-center">
-              <OKRGraph
-                show={show}
-                rowScore={data?.progressLow}
-                mediumScore={data?.progressMid}
-                highScore={data?.progressHigh}
-              />
-            </div>
-          </div>
-          {(!memberData || memberData.length === 0) && (
-            <div className="text-center my-10">
-              <p className="font-size-h5 mb-6">
-                {`${year}년 ${quarter}분기 OKR Data가 없습니다.`}
-              </p>
-            </div>
-          )}
-          {memberData &&
-            memberData.map((member: any) => (
-              <>
-                <div className="d-flex align-items-center w-100 mb-3">
-                  <Profile user={member?.user} />
-                  <div className="font-weight-bolder text-dark font-size-lg ml-3">
-                    {member?.user?.name}
-                  </div>
-                  {my.id === member?.user?.id && (
-                    <span className="label label-lg label-info font-weight-bolder ml-2">
-                      나
-                    </span>
-                  )}
-                </div>
-                <OKRAccordion
-                  objectives={member?.objective}
-                  user={member?.user}
+          <DataValidationContainer isFetching={!availableDates && isFetching}>
+            <div className="gutter-t mb-10">
+              <div className="d-flex justify-content-between align-items-center">
+                <OKRGraph
+                  show={show}
+                  rowScore={data?.progressLow}
+                  mediumScore={data?.progressMid}
+                  highScore={data?.progressHigh}
                 />
-              </>
-            ))}
+              </div>
+            </div>
+            {(!memberData || memberData.length === 0) && (
+              <div className="text-center my-10">
+                <p className="font-size-h5 mb-6">
+                  {(data?.progressLow || 0) +
+                    (data?.progressMid || 0) +
+                    (data?.progressHigh || 0) ===
+                  0
+                    ? `${year}년 ${quarter}분기 OKR Data가 없습니다.`
+                    : ``}
+                </p>
+              </div>
+            )}
+            {memberData &&
+              memberData.map((member: any) => (
+                <>
+                  <div className="d-flex align-items-center w-100 mb-3">
+                    <Profile user={member?.user} />
+                    <div className="font-weight-bolder text-dark font-size-lg ml-3">
+                      {member?.user?.name}
+                    </div>
+                    {my.id === member?.user?.id && (
+                      <span className="label label-lg label-info font-weight-bolder ml-2">
+                        나
+                      </span>
+                    )}
+                  </div>
+                  <OKRAccordion
+                    objectives={member?.objective}
+                    user={member?.user}
+                  />
+                </>
+              ))}
+          </DataValidationContainer>
         </div>
       </div>
     </div>
--- src/components/okr/UserOKR.tsx
+++ src/components/okr/UserOKR.tsx
@@ -1,10 +1,13 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useAuth, useModal } from "hooks/useRedux";
 import { useMyOKR, useUserOKR } from "hooks/useOKRRedux";
 import React, { useEffect, useState } from "react";
 import SVG from "utils/SVG";
+import DataValidationContainer from "layouts/DataValidationContainer";
 import OKRAccordion from "./OKRAccordion";
 import OKRGraph from "./OKRGraph";
+import OKRWrite from "./OKRWrite";
 
 type UserOKRType = {
   isMy?: boolean;
@@ -20,14 +23,21 @@ export default function UserOKR({ isMy }: UserOKRType) {
     extraData?: any;
   };
   let isFetching;
-  let requset: (arg0: number, arg1: number, arg2?: number) => void;
-  if (isMy) ({ data: responseData, isFetching, requset } = useMyOKR());
-  else ({ data: responseData, isFetching, requset } = useUserOKR());
+  let request: (arg0: number, arg1: number, arg2?: number) => void;
+  if (isMy) ({ data: responseData = {}, isFetching, request } = useMyOKR());
+  else ({ data: responseData = {}, isFetching, request } = useUserOKR());
+
   const { showModal } = useModal();
   const { data, availableDates, year, quarter, user } = responseData;
   const [show, setShow] = useState(false);
+  const [okrWriteType, setOkrWriteType] = useState<"write" | "add" | "update">(
+    "write"
+  );
+  const [updateOKRId, setUpdateOKRId] = useState<number>();
   const [isWrite, setIsWrite] = useState(false);
   const [isModifiable, setIsModifiable] = useState(false);
+  const [isWriteOpen, setIsWriteOpen] = useState(false);
+  const [isModifiableOpen, setIsModifiableOpen] = useState(false);
 
   useEffect(() => {
     if (responseData?.extraData) {
@@ -57,11 +67,17 @@ export default function UserOKR({ isMy }: UserOKRType) {
     setShow(true);
   }, []);
 
-  function changeDate(changeYear: number, changeQuarter: number) {
+  function onChangeDate(changeYear: number, changeQuarter: number) {
     setShow(false);
-    requset(changeYear, changeQuarter, user?.id);
+    request(changeYear, changeQuarter, user?.id);
     setTimeout(() => setShow(true), 200);
   }
+
+  const onUpdateOKR = (id: number) => {
+    setUpdateOKRId(id);
+    setOkrWriteType("update");
+    setIsWriteOpen(true);
+  };
 
   return (
     <div
@@ -142,7 +158,7 @@ export default function UserOKR({ isMy }: UserOKRType) {
             <select
               onChange={({ target }) => {
                 const [changeYear, changeQuarter] = target.value.split("_");
-                changeDate(Number(changeYear), Number(changeQuarter));
+                onChangeDate(Number(changeYear), Number(changeQuarter));
               }}
               className="custom-select form-control border-0 shadow-none pr-5 bgi-position-x-right"
             >
@@ -160,60 +176,80 @@ export default function UserOKR({ isMy }: UserOKRType) {
             </select>
           </div>
         </div>
+
         <div className="card-body pt-2 overflow-y-auto">
-          <div className="gutter-t mb-10">
-            <div className="d-flex justify-content-between align-items-center">
-              <OKRGraph
-                show={show}
-                rowScore={data?.progressLow}
-                mediumScore={data?.progressMid}
-                highScore={data?.progressHigh}
-              />
+          <DataValidationContainer isFetching={!availableDates && isFetching}>
+            <div className="gutter-t mb-10">
+              <div className="d-flex justify-content-between align-items-center">
+                <OKRGraph
+                  show={show}
+                  rowScore={data?.progressLow}
+                  mediumScore={data?.progressMid}
+                  highScore={data?.progressHigh}
+                />
+              </div>
             </div>
-          </div>
-          {!data &&
-            (isWrite && isMy ? (
-              <div className="text-center my-10">
-                <p className="font-size-h5 mb-6">
-                  {`${year}년 ${quarter}분기 OKR을 작성해 주세요.`}
-                </p>
-                <button
-                  type="button"
-                  className="btn btn-primary btn-sm"
-                  data-toggle="layer"
-                  data-target="#layer_createOKR"
-                >
-                  <span className="svg-icon">
-                    <SVG name="write" />
-                  </span>
-                  OKR 작성
-                </button>
-              </div>
-            ) : (
-              <div className="text-center my-10">
-                <p className="font-size-h5 mb-6">
-                  {`${year}년 ${quarter}분기 OKR Data가 없습니다.`}
-                </p>
-              </div>
-            ))}
-          {data && (
-            <>
-              <OKRAccordion objectives={data?.objective} user={user} />
-              {isMy && (isWrite || isModifiable) && (
-                <div className="text-center mt-n4">
-                  <a
-                    href="javascript:;"
-                    className="svg-icon svg-icon-primary svg-icon-xxl"
+            {!data ? (
+              isWrite && isMy ? (
+                <div className="text-center my-10">
+                  <p className="font-size-h5 mb-6">
+                    {`${year}년 ${quarter}분기 OKR을 작성해 주세요.`}
+                  </p>
+                  <button
+                    type="button"
+                    className="btn btn-primary btn-sm"
                     data-toggle="layer"
                     data-target="#layer_createOKR"
+                    onClick={() => {
+                      setOkrWriteType("write");
+                      setIsWriteOpen(true);
+                    }}
                   >
-                    <SVG name="plus" />
-                  </a>
+                    <span className="svg-icon">
+                      <SVG name="write" />
+                    </span>
+                    OKR 작성
+                  </button>
                 </div>
-              )}
-            </>
-          )}
+              ) : (
+                <div className="text-center my-10">
+                  <p className="font-size-h5 mb-6">
+                    {`${year}년 ${quarter}분기 OKR Data가 없습니다.`}
+                  </p>
+                </div>
+              )
+            ) : (
+              <>
+                <OKRAccordion
+                  onUpdateOKR={onUpdateOKR}
+                  objectives={data?.objective}
+                  user={user}
+                />
+                {isMy && (isWrite || isModifiable) && (
+                  <div className="text-center mt-n4">
+                    <a
+                      className="svg-icon svg-icon-primary svg-icon-xxl"
+                      onClick={() => {
+                        setOkrWriteType("add");
+                        setIsWriteOpen(true);
+                      }}
+                    >
+                      <SVG name="bigPlus" />
+                    </a>
+                  </div>
+                )}
+              </>
+            )}
+          </DataValidationContainer>
         </div>
+        <OKRWrite
+          type={okrWriteType}
+          show={isWriteOpen}
+          close={() => setIsWriteOpen(false)}
+          updateOKRId={updateOKRId}
+          year={year}
+          quarter={quarter}
+        />
       </div>
     </div>
   );
