846fe3fdbf4a9656a086a65bcfd5d8e774a40d9e
권수현 <3a9lyd@naver.com>
2021-01-13 01:47:00 +0900

    최상위 탭 영역 수정 및 리뷰 분기 선택 추가

--- src/components/main/TabItem.tsx
+++ src/components/main/TabItem.tsx
@@ -1,16 +1,51 @@
-import React from "react";
+import { useReviewMain } from "hooks/useReview";
+import React, { useEffect, useState } from "react";
+import { Dropdown, DropdownButton } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
 
 export default function TabItem({ path }: { path: string }) {
   const location = useLocation();
+  const [selectMeta, setSelectMeta] = useState<any>();
+  const { request, availableMetas = [], data = {} } = useReviewMain();
+
+  const { meta = {} } = data;
+
+  useEffect(() => {
+    if (!selectMeta) setSelectMeta(availableMetas[0]);
+  }, [availableMetas]);
+
   return (
     <li className="nav-item col-4">
       <Link
         to={`/${path}`}
         className={`nav-link ${location.pathname === `/${path}` && "active"}`}
-        data-target="#content_tab_feedback"
+        data-target="#content_tab_review"
       >
-        <span className="nav-title text-uppercase">{path || "Feedback"}</span>
+        <span className="d-flex align-items-center nav-title text-uppercase">
+          {path === "Review" ? (
+            <DropdownButton
+              className="d-flex flex-center review-tab-select"
+              title={meta?.name}
+              variant="light"
+              size="lg"
+            >
+              {availableMetas &&
+                availableMetas.map((metaData: any) => (
+                  <Dropdown.Item
+                    onClick={() => {
+                      setSelectMeta(metaData);
+                      request(metaData.id);
+                    }}
+                  >
+                    {metaData.name}
+                  </Dropdown.Item>
+                ))}
+            </DropdownButton>
+          ) : (
+            <></>
+          )}
+          {path || "Feedback"}
+        </span>
       </Link>
     </li>
   );
