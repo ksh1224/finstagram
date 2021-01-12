c205bc2be2caa426002b95dc6684e3dea8fee69c
권수현 <3a9lyd@naver.com>
2021-01-12 19:15:15 +0900

    sso 상태별 로직 별 화면 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable default-case */
+import React, { createContext } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { Provider } from "react-redux";
@@ -8,8 +10,13 @@ import * as serviceWorker from "serviceWorker";
 
 import configureStore from "store";
 import rootSaga from "store/saga";
-import AzureAD from "react-aad-msal";
+import AzureAD, { AuthenticationState } from "react-aad-msal";
 import { authProvider } from "utils/AuthProvider";
+
+export const AuthContext = createContext<{ logout: any; login: any }>({
+  logout: null,
+  login: null,
+});
 
 const store = configureStore();
 store.runSaga(rootSaga);
@@ -17,11 +24,51 @@ store.runSaga(rootSaga);
 ReactDOM.render(
   <React.StrictMode>
     <AzureAD provider={authProvider} forceLogin reduxStore={store}>
-      <Provider store={store}>
-        <Router>
-          <App />
-        </Router>
-      </Provider>
+      {({ login, authenticationState, error, logout }: any) => {
+        switch (authenticationState) {
+          case AuthenticationState.Authenticated:
+            return (
+              <AuthContext.Provider value={{ logout, login }}>
+                <Provider store={store}>
+                  <Router>
+                    <App />
+                  </Router>
+                </Provider>
+              </AuthContext.Provider>
+            );
+          case AuthenticationState.Unauthenticated:
+            return (
+              <div className="d-flex flex-row flex-column-fluid page">
+                <div className="d-flex w-100 align-items-center justify-content-center">
+                  <div>
+                    {error && (
+                      <div className="font-size-h4 text-dark-75 font-weight-bolder mb-5">
+                        에러가 발생했습니다. 다시 로그인 해주세요.
+                      </div>
+                    )}
+                    <div className="text-center">
+                      <button
+                        type="button"
+                        className="btn w-30 btn-primary"
+                        onClick={() => login && login()}
+                      >
+                        Sign In
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          case AuthenticationState.InProgress:
+            return (
+              <div className="d-flex flex-row flex-column-fluid page">
+                <div className="d-flex w-100 align-items-center justify-content-center">
+                  <div className="spinner spinner-primary spinner-lg spinner-center w-100 h-50px" />
+                </div>
+              </div>
+            );
+        }
+      }}
     </AzureAD>
   </React.StrictMode>,
   document.getElementById("root")
