3bedaf7490ce9ee7dfbc3a1248298b10590bca48
권수현 <3a9lyd@naver.com>
2021-01-13 01:34:19 +0900

    피드 아이템 수정, 삭제, 댓글 추가 기능 추가

--- src/components/item/FeedListItem.tsx
+++ src/components/item/FeedListItem.tsx
@@ -1,10 +1,15 @@
+/* eslint-disable no-nested-ternary */
 import Profile from "components/Profile";
 import SVG from "utils/SVG";
-import React, { useEffect, useState } from "react";
-import { useComment } from "hooks/useFeedBackRedux";
+import React, { useState } from "react";
+import axios from "utils/axiosUtil";
+import { useAuth, useModal } from "hooks/useRedux";
+import { useFeedOne } from "hooks/useFeedBackRedux";
 import CommentItem, { CommentDataType } from "./CommentItem";
 
 export type DataType = {
+  fileUrl?: string;
+  request?: boolean;
   category?: string;
   commentCount?: number;
   contents?: string;
@@ -35,36 +40,99 @@ export type DataType = {
   statusType?: string;
   type?: string;
   updatedAt?: string;
-  feedType?: "recent" | "received" | "sent"; // 피드 종류
+  feedType?: "recent" | "received" | "sent" | "modal"; // 피드 종류
+  feedbackComment?: {
+    content?: string;
+    createdAt?: string;
+    id?: number;
+    likeCount?: number;
+    liked?: boolean;
+    updatedAt?: string;
+    user?: {
+      id?: number;
+      username?: string;
+      name?: string;
+      profileImageUrl?: string;
+      organization?: { name?: string };
+    };
+  }[];
+  onUpdate?: () => void;
 };
 
-export default function FeedListItem({
-  id,
-  sendUser,
-  receiveUser,
-  contents,
-  feedbackBadge,
-  createdAt,
-  commentCount,
-  likeCount,
-  feedType,
-}: DataType) {
+export default function FeedListItem(feed: DataType) {
+  const {
+    id,
+    sendUser,
+    receiveUser,
+    contents,
+    feedbackBadge,
+    createdAt,
+    commentCount,
+    likeCount,
+    feedType,
+    liked,
+    feedbackComment,
+    onUpdate,
+  } = feed || {};
+  const { update, deleteFeed } = useFeedOne();
+  const { user: my } = useAuth();
+  const { showModal } = useModal();
   const [open, setOpen] = useState(false);
-  const [like, setLike] = useState(false);
-  const [commentList, setCommentList] = useState<any[]>([]);
-  const { commentRequest, comments } = useComment();
+  const [like, setLike] = useState(liked);
+  const [text, setText] = useState("");
 
-  useEffect(() => {
-    if (open) commentRequest(id);
-  }, [open]);
+  const clickLike = async () => {
+    const body = {
+      feedbackId: id,
+      like: !like,
+      userId: receiveUser?.id,
+    };
+    try {
+      await axios(`/feedbacks/like`, "POST", JSON.stringify(body));
+      setLike(!like);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
 
-  useEffect(() => {
-    comments.forEach((commentObj: { feedId: number; data: any[] }) => {
-      if (commentObj.feedId === id) {
-        setCommentList([...commentObj.data]);
+  const addComment = async () => {
+    if (text.trim() === "") alert("댓글을 입력해 주세요");
+    else {
+      try {
+        const res = await axios(`/feedbacks/comment/new`, "POST", {
+          content: text.trim(),
+          feedbackId: id,
+          userId: my.id,
+        });
+        if (res.responseCode === "SUCCESS") {
+          setText("");
+          update(id);
+          if (onUpdate) onUpdate();
+        }
+      } catch (error) {
+        const aaa = await error;
+        console.log("error", aaa);
+        alert("Error!");
       }
-    });
-  }, [comments]);
+    }
+  };
+
+  const deleteComment = async (commentId: number) => {
+    try {
+      const res = await axios(
+        `/feedbacks/comment/delete/${commentId}`,
+        "DELETE"
+      );
+      if (res.responseCode === "SUCCESS") {
+        update(id);
+        if (onUpdate) onUpdate();
+      }
+    } catch (error) {
+      const aaa = await error;
+      console.log("error", aaa);
+      alert("Error!");
+    }
+  };
 
   return (
     <div className="card card-custom gutter-b">
@@ -123,6 +191,7 @@ export default function FeedListItem({
               <button
                 type="button"
                 className="btn btn-sm btn-text-dark-50 btn-hover-icon-danger btn-hover-text-danger bg-hover-light-danger font-weight-bolder rounded font-size-sm p-2"
+                onClick={() => clickLike()}
               >
                 <span
                   className={`svg-icon svg-icon-md pr-1 ${
@@ -131,7 +200,14 @@ export default function FeedListItem({
                 >
                   <SVG name="like" />
                 </span>
-                {likeCount}
+                {typeof likeCount === "number" &&
+                  (liked
+                    ? like
+                      ? likeCount
+                      : likeCount - 1
+                    : like
+                    ? likeCount + 1
+                    : likeCount)}
               </button>
               {feedType === "sent" && (
                 <div className="dropdown dropdown-inline">
@@ -164,10 +240,22 @@ export default function FeedListItem({
                     </span>
                   </button>
                   <div className="dropdown-menu">
-                    <a className="dropdown-item" href="javascript:;">
+                    <a
+                      className="dropdown-item"
+                      href="javascript:;"
+                      onClick={() => id && deleteFeed(id)}
+                    >
                       삭제하기
                     </a>
-                    <a className="dropdown-item" href="javascript:;">
+                    <a
+                      className="dropdown-item"
+                      href="javascript:;"
+                      onClick={() =>
+                        feed.request
+                          ? showModal("updateRequestFeedback", feed)
+                          : showModal("updateSendFeedback", feed)
+                      }
+                    >
                       수정하기
                     </a>
                   </div>
@@ -181,10 +269,15 @@ export default function FeedListItem({
               className="pt-5 collapse"
               id={`cmt-feedback-feedback-${`${feedType}${id}`}`}
             >
-              {!!commentList &&
-                commentList.length !== 0 &&
-                commentList.map((comment: CommentDataType) => (
-                  <CommentItem key={comment?.id} {...comment} />
+              {!!feedbackComment &&
+                feedbackComment.length !== 0 &&
+                feedbackComment.map((comment: CommentDataType) => (
+                  <CommentItem
+                    key={`${feedType}_${comment?.id}`}
+                    {...comment}
+                    onUpdate={() => update(id)}
+                    onDelete={() => comment?.id && deleteComment(comment?.id)}
+                  />
                 ))}
               <div className="separator separator-solid mt-5 mb-4" />
               <form className="position-relative">
@@ -196,9 +289,15 @@ export default function FeedListItem({
                     overflowWrap: "break-word",
                     height: "19px",
                   }}
+                  rows={1}
+                  value={text}
+                  onChange={({ target }) => setText(target.value)}
                 />
                 <div className="position-absolute top-0 right-0 mt-n1 mr-n2">
-                  <span className="btn btn-icon btn-sm btn-hover-icon-primary">
+                  <span
+                    className="btn btn-icon btn-sm btn-hover-icon-primary"
+                    onClick={() => addComment()}
+                  >
                     <span className="svg-icon svg-icon-primary">
                       <SVG name="send" />
                     </span>
