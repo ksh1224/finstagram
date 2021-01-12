48fe088b92311ba2c0dad1e6a9046ac334e7eb81
권수현 <3a9lyd@naver.com>
2021-01-13 01:47:25 +0900

    도움말 추가

--- src/layouts/main/Body.tsx
+++ src/layouts/main/Body.tsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import SVG from "utils/SVG";
+import axios from "utils/axiosUtil";
+import { useAuth, useModal } from "hooks/useRedux";
+import { useSelector } from "react-redux";
 
 type LayoutType = {
   children: JSX.Element[];
 };
 
 export default function Body({ children }: LayoutType) {
+  const { pathname } = useLocation();
+  const { user: my } = useAuth();
+  const { showModal } = useModal();
+  const { activeHelp, activePush } = useSelector(
+    (state: RootState) => state.setting
+  );
+  const [guides, setGuides] = useState<
+    {
+      fileUrl: string;
+      title: string;
+      uri: string;
+    }[]
+  >([]);
+  useEffect(() => {
+    (async () => {
+      try {
+        let url = "/guide/feedback";
+        if (pathname === "/OKR") url = "/guide/okr";
+        else if (pathname === "/Review") url = "/guide/review";
+        const { data } = await axios(url, "GET");
+        if (pathname === "/Review" && my.isReviewer) {
+          const { evalData } = await axios("/guide/performance", "GET");
+          setGuides([...evalData, ...data]);
+        } else setGuides(data);
+      } catch (error) {
+        console.log("error", error);
+      }
+    })();
+  }, [pathname]);
   return (
     <div className="d-flex flex-row flex-column-fluid page">
       <div
@@ -13,6 +47,51 @@ export default function Body({ children }: LayoutType) {
       >
         {children}
       </div>
+      {activeHelp ? (
+        <div className="position-fixed bottom-0 right-0 mr-5 mb-5 dropleft">
+          <button
+            className="btn btn-icon btn-clean btn-lg p-0"
+            type="button"
+            id="finstagram_help"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <a
+              className="d-flex btn bg-gray-300 w-100 h-100 p-0 align-items-center justify-content-center"
+              data-toggle="tooltip"
+              data-placement="top"
+              data-theme="dark"
+              title="도움말"
+            >
+              <span className="svg-icon svg-icon-2x">
+                <SVG name="help" />
+              </span>
+            </a>
+          </button>
+          <div
+            className="dropdown-menu w-auto"
+            aria-labelledby="finstagram_help"
+          >
+            {guides.map(({ title, uri, fileUrl }) => (
+              <a
+                className="dropdown-item"
+                onClick={() =>
+                  showModal("help", {
+                    title,
+                    uri,
+                    fileUrl,
+                  })
+                }
+              >
+                {title}
+              </a>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
