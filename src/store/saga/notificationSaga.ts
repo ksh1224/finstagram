51d79e52076c97d7dc462984c808976dc25c5ed3
권수현 <3a9lyd@naver.com>
2021-02-04 14:53:01 +0900

    노티 페이징 에러, 데이터 값 변경 적용

--- src/store/saga/notificationSaga.ts
+++ src/store/saga/notificationSaga.ts
@@ -1,4 +1,4 @@
-import { call, put } from "redux-saga/effects";
+import { call, put, select } from "redux-saga/effects";
 
 import axios from "utils/axiosUtil";
 import { notificationActionAsync } from "store/actions";
@@ -7,13 +7,28 @@ export default function* notificationSaga(
   page?: number
 ): Generator<any, void, ObjectType> {
   try {
-    const data = yield call(
-      axios,
-      `/noti${page ? `?page=${page}` : ""}`,
-      "GET"
-    );
-    const { data: notiCount } = yield call(axios, `/noti/count`, "GET");
-    yield put(notificationActionAsync.success({ ...data, notiCount }));
+    if (page) {
+      const { data: prevData = [] } = yield select(
+        (state: RootState) => state.notification
+      );
+      const data = yield call(
+        axios,
+        `/noti${page ? `?page=${page}` : ""}`,
+        "GET"
+      );
+      const { data: notiCount } = yield call(axios, `/noti/count`, "GET");
+      yield put(
+        notificationActionAsync.success({
+          ...data,
+          data: [...prevData, ...data.data],
+          notiCount,
+        })
+      );
+    } else {
+      const data = yield call(axios, `/noti`, "GET");
+      const { data: notiCount } = yield call(axios, `/noti/count`, "GET");
+      yield put(notificationActionAsync.success({ ...data, notiCount }));
+    }
   } catch (error) {
     yield put(notificationActionAsync.failure(error));
   }
