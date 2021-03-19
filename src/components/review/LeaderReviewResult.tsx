fea59d341c832fb9a1b488d45a971610849d9d01
권수현 <3a9lyd@naver.com>
2021-03-19 14:54:24 +0900

    리더 리뷰 결과 데이터 벨리데이션 처리

--- src/components/review/LeaderReviewResult.tsx
+++ src/components/review/LeaderReviewResult.tsx
@@ -92,141 +92,142 @@ export default function LeaderReviewResult({
       );
     }
   }, [data]);
-
-  return (
-    <div className="mb-30 tab-group">
-      {!modalLeadership ? (
-        <h3 className="d-flex h-40px font-weight-bolder align-items-center mb-0">
-          리더 Review 결과
-        </h3>
-      ) : (
-        <h5 className="d-flex gutter-b align-items-center justify-content-center line-height-40px">
-          <span className="d-inline-block h-40px font-weight-bolder border-bottom">
+  if (modalLeadership || data?.result?.leadership)
+    return (
+      <div className="mb-30 tab-group">
+        {!modalLeadership ? (
+          <h3 className="d-flex h-40px font-weight-bolder align-items-center mb-0">
             리더 Review 결과
-            <span />
-          </span>
-        </h5>
-      )}
-      <div className="">
-        <div className="mt-10">
-          <div className="font-weight-bolder text-dark font-size-lg">
-            Overall Score(긍정 응답률)
-          </div>
-          <div className="position-relative mt-4">
-            <canvas ref={barRef} className="max-w-600px" height="50px" />
-            <div
-              className="chart-sub-value"
-              style={{ width: "calc(100% - 200px)" }}
-            >
-              <span
-                className={`d-flex align-items-center mt-4 mb-2 ${
-                  myScorePercentage <= 50
-                    ? "position-relative left-50 text-dark"
-                    : ""
-                }`}
+          </h3>
+        ) : (
+          <h5 className="d-flex gutter-b align-items-center justify-content-center line-height-40px">
+            <span className="d-inline-block h-40px font-weight-bolder border-bottom">
+              리더 Review 결과
+              <span />
+            </span>
+          </h5>
+        )}
+        <div className="">
+          <div className="mt-10">
+            <div className="font-weight-bolder text-dark font-size-lg">
+              Overall Score(긍정 응답률)
+            </div>
+            <div className="position-relative mt-4">
+              <canvas ref={barRef} className="max-w-600px" height="50px" />
+              <div
+                className="chart-sub-value"
+                style={{ width: "calc(100% - 200px)" }}
               >
-                평균 {myScoreTotalAverage}점
-              </span>
-              <span
-                className={`d-flex align-items-center mt-2 mb-4 ${
-                  averageScorePercentage <= 50
-                    ? "position-relative left-50 text-dark"
-                    : ""
-                }`}
-              >
-                평균 {averageScoreTotalAverage}점
-              </span>
+                <span
+                  className={`d-flex align-items-center mt-4 mb-2 ${
+                    myScorePercentage <= 50
+                      ? "position-relative left-50 text-dark"
+                      : ""
+                  }`}
+                >
+                  평균 {myScoreTotalAverage}점
+                </span>
+                <span
+                  className={`d-flex align-items-center mt-2 mb-4 ${
+                    averageScorePercentage <= 50
+                      ? "position-relative left-50 text-dark"
+                      : ""
+                  }`}
+                >
+                  평균 {averageScoreTotalAverage}점
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="mt-10">
-          <div className="font-weight-bolder text-dark font-size-lg">
-            문항 별 결과(긍정응답률)
-          </div>
-          <div className="card bg-light-light text-dark-75 mt-4">
-            <div
-              className="card-body review-score py-4 px-6"
-              style={{ whiteSpace: "pre-wrap" }}
-            >
-              {scores &&
-                scores.map(
-                  ({ question, myScore, averageScore }: any, i: number) => {
-                    return (
-                      <div className="d-flex py-3 align-items-center justify-content-between flex-wrap">
-                        <div className="flex-grow-1 text-dark-75 mb-0 mr-4 word-keep">
-                          {i + 1}. {question}
+          <div className="mt-10">
+            <div className="font-weight-bolder text-dark font-size-lg">
+              문항 별 결과(긍정응답률)
+            </div>
+            <div className="card bg-light-light text-dark-75 mt-4">
+              <div
+                className="card-body review-score py-4 px-6"
+                style={{ whiteSpace: "pre-wrap" }}
+              >
+                {scores &&
+                  scores.map(
+                    ({ question, myScore, averageScore }: any, i: number) => {
+                      return (
+                        <div className="d-flex py-3 align-items-center justify-content-between flex-wrap">
+                          <div className="flex-grow-1 text-dark-75 mb-0 mr-4 word-keep">
+                            {i + 1}. {question}
+                          </div>
+                          <div className="d-flex justify-content-end font-weight-bolder mt-0 text-dark-75">
+                            <div className="mr-6 p-0 text-nowrap">
+                              My Score
+                              <span className=" text-primary ml-2">
+                                {myScore}
+                              </span>
+                            </div>
+                            <div className="mr-6 p-0 text-nowrap">
+                              사업부 평균
+                              <span className="text-primary ml-2">
+                                {averageScore}
+                              </span>
+                            </div>
+                            <div className="mr-6 p-0 text-nowrap">
+                              Gap
+                              <span className="text-primary ml-2">
+                                {myScore - averageScore}
+                              </span>
+                            </div>
+                          </div>
                         </div>
-                        <div className="d-flex justify-content-end font-weight-bolder mt-0 text-dark-75">
-                          <div className="mr-6 p-0 text-nowrap">
-                            My Score
-                            <span className=" text-primary ml-2">
-                              {myScore}
-                            </span>
-                          </div>
-                          <div className="mr-6 p-0 text-nowrap">
-                            사업부 평균
-                            <span className="text-primary ml-2">
-                              {averageScore}
-                            </span>
-                          </div>
-                          <div className="mr-6 p-0 text-nowrap">
-                            Gap
-                            <span className="text-primary ml-2">
-                              {myScore - averageScore}
-                            </span>
-                          </div>
-                        </div>
-                      </div>
-                    );
-                  }
-                )}
+                      );
+                    }
+                  )}
 
-              {/*  */}
+                {/*  */}
+              </div>
             </div>
           </div>
-        </div>
-        <div className="mt-10">
-          <div className="font-weight-bolder text-dark font-size-lg">
-            Comments
+          <div className="mt-10">
+            <div className="font-weight-bolder text-dark font-size-lg">
+              Comments
+            </div>
+            {continueComments && continueComments.length !== 0 ? (
+              <div className="mt-6">
+                <div className="font-weight-bold text-dark font-size-lg">
+                  리더로서 지속적으로 유지해야할 강점을 기재해 주세요.
+                </div>
+                <div className="card bg-light-light text-dark-75 mt-2 line-height-xl">
+                  <div className="card-body p-6 word-keep">
+                    <ul className="pl-4 mt-2 mb-0">
+                      {continueComments.map((comment: string) => (
+                        <li className="word-keep">{comment}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <> </>
+            )}
+            {considerComments && considerComments.length !== 0 ? (
+              <div className="mt-6">
+                <div className="font-weight-bold text-dark font-size-lg">
+                  더 좋은 리더가 되기 위해 개발해야 하는 점을 기재해주세요.
+                </div>
+                <div className="card bg-light-light text-dark-75 mt-2 line-height-xl">
+                  <div className="card-body p-6 word-keep">
+                    <ul className="pl-4 mt-2 mb-0">
+                      {considerComments.map((comment: string) => (
+                        <li className="word-keep">{comment}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <> </>
+            )}
           </div>
-          {continueComments && continueComments.length !== 0 ? (
-            <div className="mt-6">
-              <div className="font-weight-bold text-dark font-size-lg">
-                리더로서 지속적으로 유지해야할 강점을 기재해 주세요.
-              </div>
-              <div className="card bg-light-light text-dark-75 mt-2 line-height-xl">
-                <div className="card-body p-6 word-keep">
-                  <ul className="pl-4 mt-2 mb-0">
-                    {continueComments.map((comment: string) => (
-                      <li className="word-keep">{comment}</li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            </div>
-          ) : (
-            <> </>
-          )}
-          {considerComments && considerComments.length !== 0 ? (
-            <div className="mt-6">
-              <div className="font-weight-bold text-dark font-size-lg">
-                더 좋은 리더가 되기 위해 개발해야 하는 점을 기재해주세요.
-              </div>
-              <div className="card bg-light-light text-dark-75 mt-2 line-height-xl">
-                <div className="card-body p-6 word-keep">
-                  <ul className="pl-4 mt-2 mb-0">
-                    {considerComments.map((comment: string) => (
-                      <li className="word-keep">{comment}</li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            </div>
-          ) : (
-            <> </>
-          )}
         </div>
       </div>
-    </div>
-  );
+    );
+  return <></>;
 }
